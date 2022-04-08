cf69f926ff7ec35da9a10666c663f428128fb405
yasarpkr <97381917+yasarpkr@users.noreply.github.com>
2022-04-08 12:07:15 +0300

    Delete queryCount.js

--- queryCount.js
+++
@@ -1,4 +0,0 @@
-    // Insert volte_corr_identity
-    let query_identityConfCountA = "SELECT COUNT(*) AS COUNT FROM `volte_corr_identity` WHERE service_id = 48 AND identifier = 'topon.s5s8gn-pgwth02.ugwth02.lab.node'"
-    let query_identityConfCountB = await execQuery(query_identityConfCountA,param.dbConnPar)
-    let query_identityConfCountC = query_identityConfCountB[0].COUNT
