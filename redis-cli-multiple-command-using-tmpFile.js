b9dc51fb8cef596a5dace83ad05ac1419ff630e5
yasarpkr <97381917+yasarpkr@users.noreply.github.com>
2022-04-08 13:06:09 +0300

    Update redis-cli-multiple-command-using-tmpFile.js

--- redis-cli-multiple-command-using-tmpFile.js
+++ redis-cli-multiple-command-using-tmpFile.js
@@ -9,3 +9,15 @@
     } else {
         throw new Error(`\n[${now()}] Komut çalıştırılırken hata alındı \n' + ${commandResultData_setex95_3005}`)
     }
+
+    // with FlushDb
+    let command_setex94= `\
+    printf 'FLUSHDB\\n"SETEX" "\\\\x00\\\\x00\\\\x00\\\\xd2\\\\xd1]\\\\xb9\\\\x89" "20000" "\\\\x00\\\\x01\\\\x04\`8e\\\\xed\\\\xce"' > temp.redisCmds; \
+    redis-cli -p 30001 < temp.redisCmds \
+    `
+    commandResultData_setex94 = await executeBashCommandNoLog(command_setex94,remoteFolder,param.sshServerForMadCli)
+    if(commandResultData_setex94.includes('OK\nOK')){
+        console.log(`\n[${now()}] Redis komutları başarıyla run edilmiştir`)
+    } else {
+        throw new Error(`\n[${now()}] Komut çalıştırılırken hata alındı \n' + ${commandResultData_setex94}`)
+    }
